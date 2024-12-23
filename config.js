f9fd7ba5a24cf0593fa733639c57d14ebd9a56aa
xanzho <31185551+xanzho@users.noreply.github.com>
2024-12-24 01:24:54 +0900

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@
 
 ╔════════════════════════════════════════════════════════════════════════╗
 ║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
+║  ## Created by Xanzho!                                                ║
 ║  ## Feel free to utilize any portion of the code                       ║
 ║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
 ║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
@@ -59,7 +59,7 @@ module.exports = {
 
 ╔════════════════════════════════════════════════════════════════════════╗
 ║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
+║  ## Created by Xanzho!                                                ║
 ║  ## Feel free to utilize any portion of the code                       ║
 ║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
 ║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
